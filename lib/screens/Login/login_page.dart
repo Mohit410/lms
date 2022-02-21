1f0e37bf90dc764b3b4e7fd7666bca7b354ad0dd
Mohit410 <shardams36.ms@gmail.com>
2022-02-21 22:15:53 +0530

    login form started

--- lib/screens/Login/login_page.dart
+++ lib/screens/Login/login_page.dart
@@ -1,13 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:lms/screens/Login/components/body.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  //form key
+  final _formKey = GlobalKey<FormState>();
+
+  //editing controller
+  final TextEditingController emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Body(),
+    //email field
+    final emailField = TextFormField(
+      autofocus: false,
+      keyboardType: TextInputType.emailAddress,
+      //validator: () {},
+      onSaved: (value) {
+        emailController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: const Icon(Icons.mail),
+          contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: 'Email',
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          )),
+    );
+
+    //password field
+    final passwordField = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      //validator: () {},
+      onSaved: (value) {
+        passwordController.text = value!;
+      },
+      textInputAction: TextInputAction.done,
+      decoration: InputDecoration(
+          prefixIcon: const Icon(Icons.vpn_key),
+          contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: 'Password',
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          )),
+    );
+
+    //Login Button
+    final loginButton = Material(
+      elevation: 5,
+      color: Colors.redAccent,
+      borderRadius: BorderRadius.circular(30),
+      child: MaterialButton(
+        padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+        minWidth: MediaQuery.of(context).size.width,
+        onPressed: () {},
+        child: const Text(
+          "Login",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[emailField, passwordField, loginButton],
+              )),
+        ),
+      ),
     );
   }
 }
